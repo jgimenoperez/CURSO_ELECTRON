ff22e69b4f681f14004dc6510569846c191f76ed
jgimenoperez <jgimenoperez@gmail.com>
2022-02-25 14:04:20 +0100

    Conecto con firebase

---
+++ musicfy/src/utils/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyBH-FWg5IGlfCpEebKsP3co0uk_xtu9Fqk",
+    authDomain: "musicfy-28818.firebaseapp.com",
+    projectId: "musicfy-28818",
+    storageBucket: "musicfy-28818.appspot.com",
+    messagingSenderId: "663352479822",
+    appId: "1:663352479822:web:8dfad85dd0ec18a54e731f"
+  };
+
+const firebase = initializeApp(firebaseConfig);
+export default firebase
+
+
